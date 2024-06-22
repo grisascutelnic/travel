dede042a755e1a2a6dc698d7e2004317971a25ea
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2024-06-22 21:49:40 +0300

    first comit

---
+++ src/test/java/com/application/travel_web_app/TravelWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.travel_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
